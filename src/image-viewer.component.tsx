8bdb308d3fe3d3ec0d62ed6f4f0474033713f862
malcolm <minghao_yu@163.com>
2018-08-03 11:27:59 +0800

    fix: 调整 header 文字位置

--- src/image-viewer.component.tsx
+++ src/image-viewer.component.tsx
@@ -560,7 +560,6 @@ export default class ImageViewer extends React.Component<Props, State> {
     return (
       <Animated.View style={{ zIndex: 9999 }}>
         <Animated.View style={{ ...this.styles.container, opacity: this.fadeAnim }}>
-          {this!.props!.renderHeader!(this.state.currentShowIndex)}
 
           <View style={this.styles.arrowLeftContainer}>
             <TouchableWithoutFeedback onPress={this.goBack}>
@@ -597,6 +596,8 @@ export default class ImageViewer extends React.Component<Props, State> {
           <View style={[{ bottom: 0, position: 'absolute', zIndex: 9999 }, this.props.footerContainerStyle]}>
             {this!.props!.renderFooter!(this.state.currentShowIndex)}
           </View>
+
+          {this!.props!.renderHeader!(this.state.currentShowIndex)}
         </Animated.View>
       </Animated.View>
     );
